3b4d2edd3c063a84881637086e4fe4621a81d245
abnerugeda <devabnerugeda@gmail.com>
2023-05-27 14:39:25 -0300

    Alterando a estilização do scroll bar. Colocando variáveis de cores e refatorando a fonte da página para Gandhi. Arquivo de variaveis.scss em styles.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom/client';
+import "./assets/fontes/GandhiSans-Regular.otf";
+import "./assets/fontes/GandhiSans-Bold.otf";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
