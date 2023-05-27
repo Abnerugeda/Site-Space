b91c1e974fa7fa88a015ff734034b533925cfcd4
abnerugeda <devabnerugeda@gmail.com>
2023-05-27 14:16:44 -0300

    Adicionando regra ao vscode para que ele não reclame do import-normalize do scss. /.vscode/settings.json

--- src/screens/InitialScreen/index.jsx
+++ src/screens/InitialScreen/index.jsx
@@ -1,11 +1,21 @@
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
+import banner from './banner.png';
+import styles from './InitialScreen.module.scss'
 
 export default function InitialScreen(){
     return(
         <>
             <Header/>
-            <Menu/>
+            <main>
+                <section className={styles.principal}>
+                    <Menu/>
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço"/>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
